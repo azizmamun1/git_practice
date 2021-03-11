19af44a7b232134918e41a3ad8d3dcd8971973d9
Mamun <mdazizulcse@gmail.com>
2021-03-11 22:00:57 +0600

    some think are also different

--- Hello.py
+++ Hello.py
@@ -1,10 +1,12 @@
 print("VsCode is awesome....!")
-a=10
-b=20
-x=a+b
-print("Here some is :",x)
+a = 10
+b = 20
+x = a+b
+print("Here some is :", x)
 
-for i in range(1,11):
+for i in range(1, 11):
     print(i)
 
 print("We have to do something")
+x = a*a
+print("this is a new variable", x)
