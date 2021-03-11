3eb7360c210541bc925f50a1cb8d0990d16cd8cc
Mamun <mdazizulcse@gmail.com>
2021-03-11 21:50:51 +0600

    change new string and loop

--- Hello.py
+++ Hello.py
@@ -1,9 +1,9 @@
-print("VsCode is awesome....!")
+print("VsCode is my favorite text editor")
 a=10
 b=20
 x=a+b
 print("Here some is :",x)
 
 for i in range(1,11):
-    print(i)
+    print("This valu is ganarate frequencly",i)
 
