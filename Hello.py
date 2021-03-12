c04b250f96d4b445dcae1f10bc8c5717d0766f5f
Mamun <mdazizulcse@gmail.com>
2021-03-12 12:04:14 +0600

    Here i remove loop and added triangle area

--- Hello.py
+++ Hello.py
@@ -1,9 +1,8 @@
 print("VsCode is my favorite text editor")
-a=10
-b=20
-x=a+b
-print("Here some is :",x)
+a = 10
+b = 20
+x = a+b
+print("Here some is :", x)
 
-for i in range(1,11):
-    print("This valu is ganarate frequencly",i)
-
+triangle = 0.5*a*b
+print("Triangle is :", triangle)
