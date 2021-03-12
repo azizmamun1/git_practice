e6700edda06d6e38f2cc8f4b619a7948193319f3
Mamun <mdazizulcse@gmail.com>
2021-03-12 23:40:15 +0600

    all method remove and added squre

--- Hello.py
+++ Hello.py
@@ -4,9 +4,5 @@ b = 20
 x = a+b
 print("Here some is :", x)
 
-print("We have to do something")
-x = a*a
-print("this is a new variable", x)
-
-triangle = 0.5*a*b
-print("Triangle is :", triangle)
+square=a*a
+print("Area of Squrar",square)
